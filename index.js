4cac8f241aed6446f3ef7a9ea815b9a17481c4fd
Michael <michaelthomas0721@icloud.com>
2022-05-16 14:53:43 -0400

    fix: now works with queries that have parameters

--- index.js
+++ index.js
@@ -25,7 +25,12 @@ let response = function(query) {
 }
 
 let query = function(query) {
-    return query.replace(/\{/g, (c, i, text) => text.indexOf(c) === i ? c : '{data {attributes {').replace(/[}](?=.*[}])/gs, '}}}');
+  let splitted = query.split(/\{$/gm) //Split the end '{' into an array}
+  const splitter = splitted.slice(0,2).join('{'); //Join the first two elements of the array with '{' because it was removed
+  splitted = splitted.slice(2); //Remove the first two elements of the array
+  splitted.unshift(splitter); //Add the first two elements back to the array
+  splitted = splitted.join('{data {attributes {').replace(/(\})$(?=.*[}])/gms, '}}}'); //Join the array back together an add 'data {attributes {' at join points and replace '}' at the end of lines with '}}}' (closing tags)
+  return splitted
 }
 
 module.exports = {response: response, query: query};
